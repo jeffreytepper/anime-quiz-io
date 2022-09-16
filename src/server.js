c1bd4af3c42462f8bff528fc95b1eaaabd625390
jeffreytepper <teppermax@gmail.com>
2022-09-16 18:23:23 -0500

    push working audio

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ class Round {
     }
 
     ready() {
-        clipAudio(this.filepath, 10, this.roundLength);
+        //clipAudio(this.filepath, 10, this.roundLength);
     }
 
     start() {
@@ -172,33 +172,43 @@ app.get('/', (req, res) => {
 
 app.get('/audio', (req, res) => {
 
-    const range = req.headers.range;
-    if(!range) {
-        res.status(400).send("Requires Range header");
-    }
+    // const range = req.headers.range;
+    // if(!range) {
+    //     res.status(400).send("Requires Range header");
+    // }
 
-    console.log('new audio req');
+    // console.log('new audio req');
 
-    const audioPath = '../resources/clipped/output.ogg'
-    const audioSize = fs.statSync(audioPath).size;
+    // const audioPath = '../resources/clipped/output.ogg'
+    // const audioSize = fs.statSync(audioPath).size;
 
-    const CHUNCK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNCK_SIZE, audioSize - 1);
+    // const CHUNCK_SIZE = 10 ** 6;
+    // const start = Number(range.replace(/\D/g, ""));
+    // const end = Math.min(start + CHUNCK_SIZE, audioSize - 1);
 
-    const contentLength = end - start + 1;
+    // const contentLength = end - start + 1;
+    // const headers = {
+    //     "Content-Range" : `bytes ${start}-${end}/${audioSize}`,
+    //     "Accept-Ranges" : "bytes",
+    //     "Content-Length" : contentLength,
+    //     "Content-Type" : "audio/ogg",
+    // };
+
+    // res.writeHead(206, headers);
+
+    // const audioStream = fs.createReadStream(audioPath, { start, end });
+
+    // audioStream.pipe(res);
+
     const headers = {
-        "Content-Range" : `bytes ${start}-${end}/${audioSize}`,
-        "Accept-Ranges" : "bytes",
-        "Content-Length" : contentLength,
         "Content-Type" : "audio/ogg",
+        "Cache-Control" : "no-store",
+        "Expires" : 0
     };
 
-    res.writeHead(206, headers);
+    res.set(headers);
 
-    const audioStream = fs.createReadStream(audioPath, { start, end });
-
-    audioStream.pipe(res);
+    res.sendFile('/home/max/development/aniquiz-io/resources/openings/input.ogg');
 
 });
 
